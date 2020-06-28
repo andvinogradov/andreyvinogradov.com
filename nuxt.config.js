b0c6234160fca0580d392d3acdac45de966a338c
Sergey Vinogradov <mr.vursen@gmail.com>
2020-06-28 14:49:09 +0300

    Fix meta fields

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,15 @@
 export default {
-  /*
-   ** Headers of the page
-   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
-   */
   head: {
-    title: 'Nuxt.js starter for CSB',
+    title: 'Andrey Vinogradov',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'Official Nuxt.js starter for CodeSandBox'
+        content: 'Composer, arranger, hurdy-gurdy player'
       }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    ]
   },
 
   router: {
